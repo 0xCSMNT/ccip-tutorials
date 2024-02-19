8bf9486e26a8e41c0689105c6892630092547dcb
CormacDaly <cormacdaly24@gmail.com>
2024-02-19 10:25:38 +0000

    helper config and deploy script added, works for Sepolia deployment but not verifying

--- script/DeployTT.s.sol
+++ script/DeployTT.s.sol
@@ -3,16 +3,23 @@ pragma solidity ^0.8.0;
 
 import {Script, console2} from "forge-std/Script.sol";
 import {TokenTransferor} from "../src/TokenTransferor.sol";
-
-// SEPOLIA TESTNET ADDRESS
-address constant router = 0x0BF3dE8c5D3e8A2B34D2BEeB17ABfCeBaf363A59;
-address constant linkToken = 0x779877A7B0D9E8603169DdbD7836e478b4624789;
-uint64 constant destinationChainId = 12532609583862916517;
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployTT is Script {
     TokenTransferor public tokenTransferor;
 
     function run() external {
+        // Before startBroadcast, not a real transaction
+        HelperConfig helperConfig = new HelperConfig();
+        
+        // Directly destructure the returned tuple from the getter
+        (
+            address router,
+            address linkToken,
+            uint64 destinationChainId
+        ) = helperConfig.activeNetworkConfig();
+
+        // After startBroadcast, real transaction
         vm.startBroadcast();
         tokenTransferor = new TokenTransferor(
             address(router),
--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,4 +2,50 @@
 
 pragma solidity ^0.8.0;
 
-import {Mock}
+import {MockCCIPRouter} from "test/mock/MockRouter.sol";
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig is Script {
+    NetworkConfig public activeNetworkConfig;
+
+    struct NetworkConfig {
+        address router;
+        address linkToken;
+        uint64 destinationChainId;
+    }
+
+    event HelperConfig__CreatedMockRouter(address router);
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaConfig();
+        } else {
+            activeNetworkConfig = getAnvilConfig();
+        }
+    }
+
+    function getSepoliaConfig()
+        public
+        pure
+        returns (NetworkConfig memory sepoliaNetworkConfig)
+    {
+        sepoliaNetworkConfig = NetworkConfig({
+            router: 0x0BF3dE8c5D3e8A2B34D2BEeB17ABfCeBaf363A59,
+            linkToken: 0x779877A7B0D9E8603169DdbD7836e478b4624789,
+            destinationChainId: 12532609583862916517 // Polygon Mumbai
+        });
+    }
+
+    function getAnvilConfig()
+        public
+        returns (NetworkConfig memory anvilNetworkConfig)
+    {
+        vm.startBroadcast();
+        MockCCIPRouter router = new MockCCIPRouter();
+        vm.stopBroadcast();
+
+        emit HelperConfig__CreatedMockRouter(address(router));
+
+        anvilNetworkConfig.router = address(router);
+    }
+}
